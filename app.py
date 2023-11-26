2f4e1637612838c073045ab88d384a5d8782bea2
Martyna Bławat <s184532@student.pg.edu.pl>
2023-11-26 13:22:36 +0100

    wip

--- app.py
+++ app.py
@@ -18,24 +18,19 @@ def create_directory(directory_path):
 
 
 def split_mp4_into_frame_sequences(mp4_path, output_folder, target_resolution=(1280, 720)):
-    # Create output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Open the MP4 file
     cap = cv2.VideoCapture(mp4_path)
 
-    # Get video properties
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = cap.get(cv2.CAP_PROP_FPS)
 
-    # Set the number of frames per sequence
     frames_per_sequence = 3
 
-    # Read frames and save sequences of 3 consecutive frames into folders
     for start_frame in range(0, frame_count - frames_per_sequence + 1, frames_per_sequence):
         sequence_folder = os.path.join(
-            output_folder, f"sequence_{start_frame}")
+            output_folder, f"sequence_{int(start_frame/3)}")
         os.makedirs(sequence_folder)
 
         for offset in range(frames_per_sequence):
@@ -44,14 +39,12 @@ def split_mp4_into_frame_sequences(mp4_path, output_folder, target_resolution=(1
             if not ret:
                 break
 
-            # Resize frame to the target resolution
             frame = cv2.resize(frame, target_resolution)
 
             frame_filename = f"frame_{offset:02d}.jpg"
             frame_path = os.path.join(sequence_folder, frame_filename)
             cv2.imwrite(frame_path, frame)
 
-    # Release the video capture object
     cap.release()
 
 
@@ -62,6 +55,9 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    create_directory('uploads')
+    create_directory('output')
+
     if 'file' not in request.files:
         return 'No file part'
 
@@ -69,10 +65,7 @@ def upload():
 
     if file.filename == '':
         return 'No selected file'
-
-    # Save the file to a folder of your choice
-    create_directory('uploads')
-    create_directory('output')
+    
     file.save(f'uploads/{file.filename}')
     model = keras.models.load_model('model.hdf5')
     # print(model.weights)
