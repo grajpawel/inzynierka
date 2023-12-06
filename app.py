ad4d48316090b54ae7ea450e6d782f2e37a1e860
Paweł Grajkowski <grajpawel@gmail.com>
2023-12-06 21:38:54 +0100

    wip

--- app.py
+++ app.py
@@ -48,41 +48,24 @@ def split_mp4_into_frame_sequences(mp4_path, output_folder, target_resolution=(4
             cv2.imwrite(frame_path, frame)
             images.append(frame)
 
-        # file_paths = [
-        #     os.path.join(sequence_folder, 'frame_00.jpg'),
-        #     os.path.join(sequence_folder, 'frame_01.jpg'),
-        #     os.path.join(sequence_folder, 'frame_02.jpg'),
-        #     os.path.join(sequence_folder, 'frame_03.jpg'),
-        #     os.path.join(sequence_folder, 'frame_04.jpg')]
+        file_paths = [
+            os.path.join(sequence_folder, 'frame_00.jpg'),
+            os.path.join(sequence_folder, 'frame_01.jpg'),
+            os.path.join(sequence_folder, 'frame_02.jpg'),
+            os.path.join(sequence_folder, 'frame_03.jpg'),
+            os.path.join(sequence_folder, 'frame_04.jpg')]
 
-        # frames_list = [np.array(np.asarray(Image.open(file_path), dtype='bfloat16'))
-        #                for file_path in file_paths]
+        frames_list = [np.array(np.asarray(Image.open(file_path), dtype='bfloat16'))
+                       for file_path in file_paths]
 
         frames_array = np.array(images)
         frames_array = np.expand_dims(frames_array, axis=0)
-
-        # keras_list = keras.utils.image_dataset_from_directory(
-        #     sequence_folder,
-        #     labels=None,
-        #     label_mode="int",
-        #     class_names=None,
-        #     color_mode="rgb",
-        #     batch_size=1,
-        #     image_size=(240, 426),
-        #     shuffle=False,
-        #     seed=None,
-        #     validation_split=None,
-        #     subset=None,
-        #     interpolation="bilinear",
-        #     follow_links=False,
-        #     crop_to_aspect_ratio=False)
-
-        # keras_list = np.expand_dims(keras_list, axis=0)
 
         result = model.predict(frames_array, verbose=0)
 
         if (result[0][1] > 0.6):
             print(result)
+            print(sequence_folder)
 
     cap.release()
 
